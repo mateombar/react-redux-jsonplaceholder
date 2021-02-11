2fb8f6ec235e1352ec364b940c46627dd9ccdb58
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-02-11 07:43:32 -0500

    Simple table

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,27 @@ import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HOLA</h1>
-    </div>
+    <table className="table">
+      <thead className="table_header">
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Link</th>
+        </tr>
+      </thead>
+      <tbody className="table_body">
+        <tr>
+          <td>Mateo</td>
+          <td>mateomontabaron@gmail.com</td>
+          <td><a href="https://rockers-c5e8f.web.app/">Rockers</a></td>
+        </tr>
+        <tr>
+          <td>Lupita</td>
+          <td>mateomontabaron@gmail.com</td>
+          <td><a href="https://rockers-c5e8f.web.app/">Rockers</a></td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
