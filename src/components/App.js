4cf0c689c9b29ca7314411fae4b83527b13a534a
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-02-11 15:13:32 -0500

    Print users

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,50 @@
+import React, { Component } from 'react';
 import './styles/App.css';
 
-function App() {
-  return (
-    <table className="table">
-      <thead className="table_header">
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Link</th>
-        </tr>
-      </thead>
-      <tbody className="table_body">
-        <tr>
-          <td>Mateo</td>
-          <td>mateomontabaron@gmail.com</td>
-          <td><a href="https://rockers-c5e8f.web.app/">Rockers</a></td>
-        </tr>
-        <tr>
-          <td>Lupita</td>
-          <td>mateomontabaron@gmail.com</td>
-          <td><a href="https://rockers-c5e8f.web.app/">Rockers</a></td>
-        </tr>
-      </tbody>
-    </table>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: [
+        {
+          name: 'Mateo Montanez',
+          email: 'mateomontabaron@gmail.com',
+          link: 'https://rockers-c5e8f.web.app/',
+        },
+        {
+          name: 'Lupita Lopez',
+          email: 'lupita@gmail.com',
+          link: 'https://rockers-c5e8f.web.app/',
+        },
+      ]
+    }
+  }
+  putRows = () => (
+    this.state.users.map(user => (
+      < tr >
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td><a href={user.link}>Rockers</a></td>
+      </tr >
+    ))
   );
+  render() {
+
+    return (
+      <table className="table">
+        <thead className="table_header">
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody className="table_body">
+          {this.putRows()}
+        </tbody>
+      </table>
+    );
+  }
 }
 
 export default App;
