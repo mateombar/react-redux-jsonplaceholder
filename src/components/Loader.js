cdb8ef358e995119c227697c03d68b3c28fab65d
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-02-22 18:31:07 -0500

    Loadder component

---
+++ src/components/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles/Loader.css';
+const Loader = () => (
+    <div className="center">
+        <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    </div>
+)
+
+export default Loader;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 // Allows to connect a component with a reducer
 import { connect } from 'react-redux';
-import './Users.css';
 import * as usersAction from '../../actions/usersAction';
+
+import Loader from '../Loader';
+import './Users.css';
 
 class Users extends Component {
   async componentDidMount() {
     this.props.getTodos();
   }
-
+  putContent = () => {
+    if (this.props.loading) {
+      return (<Loader />)
+    } else {
+      return (
+        <table className="table">
+          <thead className="table_header">
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Link</th>
+            </tr>
+          </thead>
+          <tbody className="table_body">
+            {
+              this.putRows()
+            }
+          </tbody>
+        </table>
+      )
+    }
+  }
   putRows = () => this.props.users.map((user) => (
     < tr key={user.id}>
       <td>{user.name}</td>
@@ -24,23 +47,7 @@ class Users extends Component {
       )
     }
     return (
-      this.props.loading ?
-        (<h3>Loading</h3>)
-        :
-        (<table className="table">
-          <thead className="table_header">
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Link</th>
-            </tr>
-          </thead>
-          <tbody className="table_body">
-            {
-              this.putRows()
-            }
-          </tbody>
-        </table>)
+      this.putContent()
     );
   }
 }
