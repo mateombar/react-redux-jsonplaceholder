4ea28a4cbae5344c9971e8682123594b159a0e80
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-02-23 10:04:22 -0500

    Using response.ok to catch the error in the fetch api. Also removing up Axios

--- src/actions/usersAction.js
+++ src/actions/usersAction.js
@@ -1,39 +1,39 @@
-import axios from 'axios';
 import { GET_TODOS, LOADING, ERROR } from '../types/usersTypes';
 
 export const getTodos = () => async (dispatch) => {
     dispatch({
         type: LOADING,
     })
-    // await fetch('https://jsonplaceholder.typicode.com/usersSSS/')
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         console.log(data);
-    //         dispatch({
-    //             type: GET_TODOS,
-    //             payload: data
-    //         })
-    //     })
-    //     .catch(error => {
-    //         console.log('error??????????');
-    //         dispatch({
-    //             type: ERROR,
-    //             payload: error.message
-    //         })
-    //     })
     try {
-        // const data = await fetch('https://jsonplaceholder.typicode.com/usersSSS/');
-        // const response = await data.json();
-        const response = await axios.get('https://jsonplaceholder.typicode.com/usersS/')
-        await dispatch({
+        // await fetch('https://jsonplaceholder.typicode.com/users/')
+        //     .then(response => {
+        //         console.log(response.status);
+        //         if (!response.ok) {
+        //             throw new Error("HTTP status " + response.status);
+        //         }
+        //         return response.json()
+        //     })
+        //     .then(data => {
+        //         dispatch({
+        //             type: GET_TODOS,
+        //             payload: data
+        //         })
+        //     })
+        const myRequest = new Request('https://jsonplaceholder.typicode.com/users/');
+        const response = await fetch(myRequest);
+        console.log(response.status);
+        if (!response.ok) {
+            throw new Error("HTTP status " + response.status);
+        }
+        const data = await response.json();
+        dispatch({
             type: GET_TODOS,
-            payload: response.data
+            payload: data
         })
     } catch (error) {
         await dispatch({
             type: ERROR,
             payload: error.message
         })
-        console.log('error??????????');
     }
 }
