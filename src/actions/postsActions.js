94200980e8c5fe70a43bbe5db8bfac45418d0041
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-02-24 11:40:50 -0500

    Avoid second searches

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -1,30 +1,11 @@
-import { GET_TODOS, LOADING, ERROR } from '../types/postsTypes';
-export const getTodos = () => async (dispatch) => {
-    dispatch({
-        type: LOADING
-    })
-    try {
-        const response = await fetch(`http://jsonplaceholder.typicode.com/posts/`)
-        if (!response.ok) {
-            throw new Error(`HTTP Status: ${response.status}`)
-        }
-        const data = await response.json();
-        dispatch({
-            type: GET_TODOS,
-            payload: data
-        })
-    } catch (error) {
-        dispatch({
-            type: ERROR,
-            payload: error.message
-        })
-    }
-}
+import { GET_BY_USER, LOADING, ERROR } from '../types/postsTypes';
 
 export const getByUser = (userId) => async (dispatch, getState) => {
     // To access to the state of an reducer from an Action
     const { users } = getState().usersReducer;
+    const { posts } = getState().postsReducer;
     const user_id = users[userId - 1].id;
+
     dispatch({
         type: LOADING
     })
@@ -34,9 +15,13 @@ export const getByUser = (userId) => async (dispatch, getState) => {
             throw new Error(`HTTP Status: ${response.status}`)
         }
         const data = await response.json();
+        const post_updated = [
+            ...posts, 
+            data
+        ]
         dispatch({
-            type: GET_TODOS,
-            payload: data
+            type: GET_BY_USER,
+            payload: post_updated
         })
     } catch (error) {
         dispatch({
--- src/types/postsTypes.js
+++ src/types/postsTypes.js
@@ -1,3 +1,3 @@
-export const GET_TODOS = 'posts_get_todos';
+export const GET_BY_USER = 'posts_get_by_user';
 export const LOADING = 'posts_loading';
 export const ERROR = 'posts_error';
