4ae368f7c93ea76d889346489a298f7ba4225b96
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-02-24 15:37:52 -0500

    immutability

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -1,28 +1,41 @@
 import { GET_BY_USER, LOADING, ERROR } from '../types/postsTypes';
+import * as usersTypes from '../types/usersTypes';
 
-export const getByUser = (userId) => async (dispatch, getState) => {
+const { GET_TODOS: USERS_GET_TODOS } = usersTypes;
+
+
+export const getByUser = (u_id) => async (dispatch, getState) => {
     // To access to the state of an reducer from an Action
     const { users } = getState().usersReducer;
     const { posts } = getState().postsReducer;
-    const user_id = users[userId - 1].id;
 
     dispatch({
         type: LOADING
     })
     try {
-        const response = await fetch(`http://jsonplaceholder.typicode.com/posts?userId=${user_id}`)
+        const response = await fetch(`http://jsonplaceholder.typicode.com/posts?userId=${u_id}`)
         if (!response.ok) {
             throw new Error(`HTTP Status: ${response.status}`)
         }
         const data = await response.json();
         const post_updated = [
-            ...posts, 
+            ...posts,
             data
         ]
+        const post_key_last = post_updated.length - 1
+        const users_updated = [...users];
+        users_updated[u_id - 1] = {
+            ...users[u_id - 1],
+            post_key_last
+        }
+        dispatch({
+            type: USERS_GET_TODOS,
+            payload: users_updated
+        });
         dispatch({
             type: GET_BY_USER,
             payload: post_updated
-        })
+        });
     } catch (error) {
         dispatch({
             type: ERROR,
